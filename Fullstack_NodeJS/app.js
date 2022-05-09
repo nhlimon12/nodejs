91459554312a2823b8a7cf529647fec6aa6d10be
nhlimon12 <nhlimon4554@gmail.com>
2022-05-09 21:34:49 +0600

    Commenting Everything

--- Fullstack_NodeJS/app.js
+++ Fullstack_NodeJS/app.js
@@ -44,41 +44,41 @@
 
 
 
-const http = require('http');
-const querystring = require('querystring');
-const port = process.env.PORT || 3000;
-const sendPlainText = (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello there. I am Nahid Hasan Limon');
-};
-const sendJsonData = (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({name : "Nahid Hasan Limon", age : 20, address : "Barishal, Bangladesh"}));
-};
-const echoData = (req, res) => {
-    const {input = ''} = querystring.parse(
-        req.url.split('?').slice(1).join('')
-    );
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(
-        {
-            normal : input,
-            upperCase : input.toUpperCase(),
-            length : input.length,
-            backwards : input.split('').reverse().join('')
-        }
-    ))
-};
-const errorData = (req, res) => {
-    res.writeHead(404, {'Content-Type' : 'text/html'});
-    res.end('<center><h1>404 Not Found!</h1></center>');
-};
+// const http = require('http');
+// const querystring = require('querystring');
+// const port = process.env.PORT || 3000;
+// const sendPlainText = (req, res) => {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello there. I am Nahid Hasan Limon');
+// };
+// const sendJsonData = (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify({name : "Nahid Hasan Limon", age : 20, address : "Barishal, Bangladesh"}));
+// };
+// const echoData = (req, res) => {
+//     const {input = ''} = querystring.parse(
+//         req.url.split('?').slice(1).join('')
+//     );
+//     res.setHeader('Content-Type', 'application/json')
+//     res.end(JSON.stringify(
+//         {
+//             normal : input,
+//             upperCase : input.toUpperCase(),
+//             length : input.length,
+//             backwards : input.split('').reverse().join('')
+//         }
+//     ))
+// };
+// const errorData = (req, res) => {
+//     res.writeHead(404, {'Content-Type' : 'text/html'});
+//     res.end('<center><h1>404 Not Found!</h1></center>');
+// };
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') return sendPlainText(req, res);
-    if (req.url === '/json') return sendJsonData(req, res);
-    if (req.url.match(/^\/echo/)) return echoData(req, res);
-    errorData(req, res);
-});
-server.listen(port);
-console.log('Your server is running on port : ' + port);
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') return sendPlainText(req, res);
+//     if (req.url === '/json') return sendJsonData(req, res);
+//     if (req.url.match(/^\/echo/)) return echoData(req, res);
+//     errorData(req, res);
+// });
+// server.listen(port);
+// console.log('Your server is running on port : ' + port);
